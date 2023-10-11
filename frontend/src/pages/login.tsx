2c7edc7f293a86b8f28317e23e9c8b1005e510f7
Pratik <pratiksolanki2026@gmail.com>
2023-10-11 20:55:43 +0530

    created Login and signup Page from the scratch

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,9 +1,66 @@
-import React from 'react'
-
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="bg-[#f9fafb] flex gap-8 flex-col justify-center items-center min-h-screen min-w-screen">
+      <div className="text-3xl font-semibold">Welcome to ChatterBox!</div>
+      <div className="p-8 bg-slate-100 shadow-md rounded-md flex flex-col items-center">
+        <form action="" method="post" className="w-96 flex flex-col gap-4">
+          <label htmlFor="username" className="font-medium text-gray-700">
+            Username
+          </label>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            className="px-2 w-full border-2 rounded-md outline-2 py-1.5 focus:outline-indigo-600"
+          />
 
-export default Login
+          <label htmlFor="password" className="font-medium text-gray-700">
+            Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            className="px-2 w-full border-2 rounded-md outline-2 py-1.5 focus:outline-indigo-600"
+          />
+
+          <button
+            type="submit"
+            className="w-full mt-4 bg-[#4f46e5] text-white font-medium rounded-md outline-2 py-1.5 hover:bg-indigo-500"
+          >
+            Login
+          </button>
+
+          <div className="flex gap-2 justify-end">
+            <span className=" text-gray-700">Didn't sign up?</span>
+            <a
+              href="/register"
+              className=" text-violet-700 font-semibold cursor-pointer"
+            >
+              Register
+            </a>
+          </div>
+          <div className="flex justify-center items-center gap-4">
+            <hr className=" w-28 "></hr>
+            <span className="text-gray-700 font-medium mb-1.5">
+              continue with
+            </span>
+            <hr className=" w-28"></hr>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="w-full mt-4 bg-gray-800 text-white font-medium rounded-md outline-2 py-1.5 hover:bg-gray-700 flex justify-center items-center gap-2"
+            >
+              <img src="./google.svg" alt="google-icon" className="w-6" />
+              <span>Login With Google</span>
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- frontend/src/pages/register.tsx
+++ frontend/src/pages/register.tsx
@@ -1,9 +1,68 @@
-import React from 'react'
-
 const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <div className="bg-[#f9fafb] flex gap-8 flex-col justify-center items-center min-h-screen min-w-screen">
+      <div className="text-3xl font-semibold">Register Your Account</div>
+      <div className="p-8 bg-slate-100 shadow-md rounded-md flex flex-col items-center">
+        <form action="" method="post" className="w-96 flex flex-col gap-4">
+          <label htmlFor="username" className="font-medium">
+            Username
+          </label>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            className="px-2 w-full border-2 rounded-md outline-2 py-1.5 focus:outline-indigo-600"
+          />
 
-export default Register
+          <label htmlFor="email" className="font-medium">
+            Email Address
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            className="px-2 w-full border-2 rounded-md outline-2 py-1.5 focus:outline-indigo-600"
+          />
+          <label htmlFor="password" className="font-medium">
+            Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            className="px-2 w-full border-2 rounded-md outline-2 py-1.5 focus:outline-indigo-600"
+          />
+
+          <label htmlFor="confirmPassword" className="font-medium">
+            Confirm Password
+          </label>
+          <input
+            type="password"
+            name="confirmPassword"
+            id="confirmPassword"
+            className="px-2 w-full border-2 rounded-md outline-2 py-1.5 focus:outline-indigo-600"
+          />
+
+          <button
+            type="submit"
+            className="w-full mt-4 bg-[#4f46e5] text-white font-medium rounded-md outline-2 py-1.5 hover:bg-indigo-500"
+          >
+            Register
+          </button>
+
+          <div className="flex gap-4 justify-center">
+            <span className=" text-gray-700">Already have an account?</span>
+            <a
+              href="/login"
+              className=" text-violet-700 font-semibold cursor-pointer"
+            >
+              Login
+            </a>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
